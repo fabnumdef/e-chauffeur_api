5726f215f980e9458124586f70764debab557f50
Gaël <gael+git@demette.fr>
2018-11-30 16:13:41 +0100

    Create api route

---
+++ routes/index.mjs
@@ -0,0 +1,20 @@
+import Router from 'koa-router';
+import users from './users';
+import pois from './pois';
+import roles from './roles';
+import rights from './rights';
+import cars from './cars';
+import jwt from './jwt';
+import campus from './campuses';
+
+const router = new Router();
+
+router.use('/users', users);
+router.use('/pois', pois);
+router.use('/roles', roles);
+router.use('/rights', rights);
+router.use('/cars', cars);
+router.use('/jwt', jwt);
+router.use('/campuses', campus);
+
+export default router.routes();
