d91418fd71ee0789ca5a06f0a17dab1edd26811d
Gaël <gael+git@demette.fr>
2018-11-30 15:48:05 +0100

    Create middleware to make pagination parsing easier

---
+++ middlewares/range-pagination.mjs
@@ -0,0 +1,21 @@
+import qs from 'qs';
+
+export default async (ctx, next) => {
+  ctx.setRangePagination = (entity, { total, offset, count }) => {
+    const name = entity.modelName.toLowerCase();
+    ctx.set('Accept-Ranges', name);
+    ctx.set('Content-Range', `${name} ${offset}-${Math.max(offset + count - 1, 0)}/${total}`);
+  };
+
+  ctx.parseRangePagination = (entity, { max = 30 } = {}) => {
+    const name = entity.modelName.toLowerCase();
+    const range = qs.parse(ctx.headers.range || '')[name] || '';
+    const [lower, higher] = range.split('-').map(n => parseInt(n, 10));
+    return {
+      offset: lower || 0,
+      limit: Math.min(higher ? higher - lower + 1 : max) || max,
+    };
+  };
+
+  await next();
+};
