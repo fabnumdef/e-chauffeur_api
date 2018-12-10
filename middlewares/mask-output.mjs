2e778ee4c0a9a680fd6d00a0cc5405bcc112d4e9
Gaël <gael+git@demette.fr>
2018-12-10 17:02:24 +0100

    More versatile maskOutput

--- middlewares/mask-output.mjs
+++ middlewares/mask-output.mjs
@@ -1,24 +1,30 @@
 import mask from 'json-mask';
+import transform from 'lodash.transform';
+import isPlainObject from 'lodash.isplainobject';
 
 function cleanObject(e) {
+  if (!isPlainObject(e) && !Array.isArray(e)) {
+    return e;
+  }
   let item = e;
-  if (item.toCleanObject) {
+
+  if (e.toCleanObject) {
     item = e.toCleanObject();
-  } else if (item.toObject) {
+  } else if (e.toObject) {
     item = e.toObject();
   }
-  item.id = item._id;
-  delete item._id;
-  delete item.__v;
-  return item;
+
+  return transform(item, (acc, val, key) => {
+    if (['__v'].includes(key)) {
+      return;
+    }
+    acc[key.replace ? key.replace('_', '') : key] = cleanObject(val);
+  });
 }
 
 export default async (ctx, next) => {
   await next();
   if (typeof ctx.body === 'object') {
-    if (Array.isArray(ctx.body)) {
-      ctx.body = ctx.body.map(cleanObject);
-    }
     ctx.body = cleanObject(ctx.body);
     ctx.body = mask(ctx.body, (ctx.query || {}).mask || ',');
   }
