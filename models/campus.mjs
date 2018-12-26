29a232b0e3d23b9075c8b040fbe218af60038529
Gaël <gael+git@demette.fr>
2018-12-26 12:47:32 +0100

    Configure categories in campus

--- models/campus.mjs
+++ models/campus.mjs
@@ -5,6 +5,10 @@ const { Schema } = mongoose;
 const CampusSchema = new Schema({
   _id: String,
   name: { type: String, required: true },
+  categories: [{
+    _id: { type: String, required: true, alias: 'id' },
+    label: String,
+  }],
 });
 
 CampusSchema.index({
