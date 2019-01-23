7899b1131d5d6530fe645814d085f7543c4ffe54
sebteil <46899994+sebteil@users.noreply.github.com>
2019-01-23 11:24:48 +0100

    Unit tests on car models : create, update, read
    Implementation of unit tests for car models
    
    Ref #1

--- tests/routes/car-models.test.mjs
+++ tests/routes/car-models.test.mjs
@@ -4,18 +4,38 @@ import request from '../request';
 const { expect } = chai;
 
 describe('Test the car models', () => {
-  // it('It should response the POST method', async () => {
-  //   const response = await request().post('/campuses').send({
-  //     id: 'TEST_TEXT_ID',
-  //     name: 'name',
-  //   });
-  //   expect(response.statusCode).to.equal(200);
-  // });
+  // Test the creation of a car model
+  it('It should response the POST method', async () => {
+     const response = await request().post('/car-models').send({
+       // ID Car-model
+       id: 'BRL_CAR_MOD_999999',
+       //
+       label: 'RENAULT ZO',
+     });
+     expect(response.statusCode).to.equal(200);
+  });
 
+  // Test the update of a car model
+  it('It should response the PATCH method', async () => {
+    const response = await request().patch('/car-models/BRL_CAR_MOD_999999').send({
+      // ID Car-model
+      id: 'BRL_CAR_MOD_999999',
+      //
+      label: 'RENAULT ZOE',
+    });
+    expect(response.statusCode).to.equal(200);
+  });
+
+  // Test the reading of a car model
   it('It should response the GET method', async () => {
     const response = await request().get('/car-models');
     expect(response.statusCode).to.equal(200);
   });
 
-  // Test create of a car-models
+  // Test the reading of a car model by its ID
+  it('It should response the GET method', async () => {
+    const response = await request().get('/car-models/BRL_CAR_MOD_999999');
+    expect(response.statusCode).to.equal(200);
+  });
+
 });
