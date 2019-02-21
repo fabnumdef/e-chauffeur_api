4fc6f014c2d409d8ec56afed3cf9f99edcc0639f
Gaël <gael+git@demette.fr>
2019-02-22 00:09:26 +0100

    Add some transactional SMS

--- services/twilio.mjs
+++ services/twilio.mjs
@@ -1,7 +1,19 @@
-import twilio from 'twilio';
+import Twilio from 'twilio';
 import config from './config';
 
 const sid = config.get('twilio:sid');
 const token = config.get('twilio:token');
+const messagingServiceSid = config.get('twilio:messagingServiceSid');
 
-export default sid && token ? twilio(sid, token) : null;
+// eslint-disable-next-line import/prefer-default-export
+export async function sendSMS(to, body) {
+  if (!sid || !token || !messagingServiceSid) {
+    throw new Error('sid nor token nor sender is missing');
+  }
+  const client = new Twilio(sid, token);
+  return client.messages.create({
+    body,
+    to,
+    messagingServiceSid,
+  });
+}
