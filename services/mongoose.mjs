63eebf83cccc659215d4c3d5e7c88473b967dc69
Gaël <gael+git@demette.fr>
2019-02-22 00:55:43 +0100

    Add retry behavior for mongoose

--- services/mongoose.mjs
+++ services/mongoose.mjs
@@ -1,5 +1,17 @@
 import mongoose from 'mongoose';
 
-export default async (config) => {
-  mongoose.connect(config, { useNewUrlParser: true });
+const connect = async (config) => {
+  try {
+    await mongoose.connect(config, { useNewUrlParser: true });
+    // eslint-disable-next-line no-console
+    console.log('MongoDB connected !');
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error('MongoDB connect failed, retry in 10 seconds');
+    await new Promise((accept) => {
+      setTimeout(accept, 10000);
+    });
+    await connect(config);
+  }
 };
+export default connect;
