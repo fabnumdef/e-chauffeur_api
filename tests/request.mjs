cff4df8b25436e340bab32ac56a0602323d574f6
Gaël <gael+git@demette.fr>
2019-03-21 13:15:50 +0100

    Update tests

--- tests/request.mjs
+++ tests/request.mjs
@@ -3,17 +3,27 @@ import config from '../services/config';
 import MongooseService from '../services/mongoose';
 import app from '../app';
 import User from '../models/user';
+import * as roles from '../models/role';
+
+const rolesKeys = {
+  ...Object.keys(roles)
+    .map(r => ({ [r]: r }))
+    .reduce((acc, r) => Object.assign(acc, r), {}),
+};
 
 MongooseService(`${config.get('mongodb')}-test`);
 
 export default () => request(app.listen());
 
-export const generateUserJWTHeader = (rights) => {
+export const generateRoleJWTHeader = (role) => {
   const user = new User({
-    cachedRights: [{
-      rights,
+    roles: [{
+      role,
     }],
   });
   const token = user.emitJWT();
   return ['Authorization', `Bearer ${token}`];
 };
+
+export const generateRegulatorJWTHeader = (...params) => generateRoleJWTHeader(rolesKeys.ROLE_REGULATOR, ...params);
+export const generateSuperAdminJWTHeader = (...params) => generateRoleJWTHeader(rolesKeys.ROLE_SUPERADMIN, ...params);
