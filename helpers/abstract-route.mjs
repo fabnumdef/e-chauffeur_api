b7304fab2d34889218508d0df674849d01c6d124
Gaël <gael+git@demette.fr>
2019-03-28 00:02:09 +0100

    Add main bypass & missing log on list

--- helpers/abstract-route.mjs
+++ helpers/abstract-route.mjs
@@ -3,7 +3,7 @@ import maskOutput from '../middlewares/mask-output';
 import checkRights from '../middlewares/check-rights';
 import addFilter from '../middlewares/add-filter';
 
-export function addCreateToRouter(Model, { url = '/', right } = {}) {
+export function addCreateToRouter(Model, { url = '/', right, main } = {}) {
   if (!right) {
     throw new Error('Right should be defined');
   }
@@ -12,7 +12,7 @@ export function addCreateToRouter(Model, { url = '/', right } = {}) {
     url,
     checkRights(right),
     maskOutput,
-    async (ctx) => {
+    main || (async (ctx) => {
       const { request: { body } } = ctx;
 
       if (!autoGenId) {
@@ -24,11 +24,11 @@ export function addCreateToRouter(Model, { url = '/', right } = {}) {
       }
       ctx.body = await Model.create(body);
       ctx.log(ctx.log.INFO, `${Model.modelName} "${body.id}" has been created`);
-    },
+    }),
   );
 }
 
-export function addListToRouter(Model, { url = '/', right, filters = {} } = {}) {
+export function addListToRouter(Model, { url = '/', right, main, filters = {}, middlewares = [] } = {}) {
   if (!right) {
     throw new Error('Right should be defined');
   }
@@ -38,23 +38,28 @@ export function addListToRouter(Model, { url = '/', right, filters = {} } = {}) 
     checkRights(right),
     maskOutput,
     ...Object.keys(filters).map(k => addFilter(k, filters[k])),
-    async (ctx) => {
+    ...middlewares,
+    main || (async (ctx) => {
       const { offset, limit } = ctx.parseRangePagination(Model);
       const total = await Model.countDocuments(ctx.filters);
       const data = await Model.find(ctx.filters).skip(offset).limit(limit).lean();
+      ctx.log(
+        ctx.log.INFO,
+        `Find query in ${Model.modelName}`,
+        { filters: ctx.filters, offset, limit },
+      );
       ctx.setRangePagination(Model, { total, offset, count: data.length });
-
       ctx.body = data;
-    },
+    }),
   );
 }
 
-export function addGetToRouter(Model, { url = '/:id', right } = {}) {
+export function addGetToRouter(Model, { url = '/:id', right, main } = {}) {
   this.get(
     url,
     checkRights(right),
     maskOutput,
-    async (ctx) => {
+    main || (async (ctx) => {
       const { params: { id } } = ctx;
       try {
         ctx.body = await Model.findById(id).lean();
@@ -65,15 +70,15 @@ export function addGetToRouter(Model, { url = '/:id', right } = {}) {
       } catch (e) {
         ctx.throw_and_log(404, `${Model.modelName} "${id}" not found`);
       }
-    },
+    }),
   );
 }
 
-export function addDeleteToRouter(Model, { url = '/:id', right } = {}) {
+export function addDeleteToRouter(Model, { url = '/:id', right, main } = {}) {
   this.del(
     url,
     checkRights(right),
-    async (ctx) => {
+    main || (async (ctx) => {
       const { params: { id } } = ctx;
       await Model.remove({ _id: id });
       ctx.log(
@@ -81,16 +86,16 @@ export function addDeleteToRouter(Model, { url = '/:id', right } = {}) {
         `${Model.modelName} "${id}" has been removed`,
       );
       ctx.status = 204;
-    },
+    }),
   );
 }
 
-export function addUpdateToRouter(Model, { url = '/:id', right } = {}) {
+export function addUpdateToRouter(Model, { url = '/:id', right, main } = {}) {
   this.patch(
     url,
     checkRights(right),
     maskOutput,
-    async (ctx) => {
+    main || (async (ctx) => {
       const { request: { body } } = ctx;
 
       const { params: { id } } = ctx;
@@ -103,7 +108,7 @@ export function addUpdateToRouter(Model, { url = '/:id', right } = {}) {
         `${Model.modelName} "${id}" has been modified`,
         { body },
       );
-    },
+    }),
   );
 }
 
