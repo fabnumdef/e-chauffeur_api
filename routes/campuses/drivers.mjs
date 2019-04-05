72bfae95b95bb100018b368e1fa10f481569ca00
RamBoFe <email@email.com>
2019-04-05 11:28:50 +0200

    Feature/drivers : add route for creating user.

--- routes/campuses/drivers.mjs
+++ routes/campuses/drivers.mjs
@@ -3,7 +3,10 @@ import Campus from '../../models/campus';
 import maskOutput from '../../middlewares/mask-output';
 import { ensureThatFiltersExists } from '../../middlewares/query-helper';
 import { checkCampusRights } from '../../middlewares/check-rights';
-import { CAN_GET_CAMPUS_DRIVER, CAN_LIST_CAMPUS_DRIVER } from '../../models/rights';
+import {
+  CAN_CREATE_CAMPUS_DRIVER, CAN_GET_CAMPUS_DRIVER, CAN_LIST_CAMPUS_DRIVER,
+} from '../../models/rights';
+import User from '../../models/user';
 
 const router = new Router();
 
@@ -35,6 +38,35 @@ router.get(
   maskOutput,
   async (ctx) => {
     ctx.body = await Campus.findDriver(ctx.params.campus_id, ctx.params.id);
+  },
+);
+
+router.post(
+  '/',
+  checkCampusRights(CAN_CREATE_CAMPUS_DRIVER),
+  maskOutput,
+  async (ctx) => {
+    const { request: { body } } = ctx;
+    if (!body.password) {
+      delete body.password;
+    }
+    if (await User.findOne({ email: body.email })) {
+      ctx.throw(409, 'User email already existing.');
+    }
+
+    const campus = await Campus.findById(ctx.params.campus_id, 'name').lean();
+    Object.assign(body,
+      {
+        roles:
+          [
+            {
+              role: 'ROLE_DRIVER',
+              campuses: [campus],
+            },
+          ],
+      });
+
+    ctx.body = await User.create(body);
   },
 );
 
