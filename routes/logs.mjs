f68978be0e963c18a72e074b769b9cf819e06c29
Gaël <gael+git@demette.fr>
2019-04-23 13:37:57 +0200

    Refactor logs route to use generic CRUD and fakeModel

--- routes/logs.mjs
+++ routes/logs.mjs
@@ -1,51 +1,43 @@
-import Router from 'koa-router';
-import maskOutput from '../middlewares/mask-output';
-
-import Poi from '../models/poi';
-import checkRights from '../middlewares/check-rights';
+import generateCRUD from '../helpers/abstract-route';
+import logger from '../services/logger';
 import {
   CAN_LIST_LOG,
 } from '../models/rights';
-import logger from '../services/logger';
 
-const router = new Router();
+const fakeModel = { modelName: 'log' };
+const router = generateCRUD(fakeModel, {
+  list: {
+    right: CAN_LIST_LOG,
+    async main(ctx) {
+      const { offset, limit } = ctx.parseRangePagination(fakeModel);
 
-router.get(
-  '/',
-  checkRights(CAN_LIST_LOG),
-  maskOutput,
-  async (ctx) => {
-    const searchParams = { ...ctx.filters };
-    if (ctx.query && ctx.query.search) {
-      searchParams.$or = [
+      const { mongodb: data } = await new Promise(
+        (resolve, reject) => logger.query(
+          {
+            limit,
+            start: offset,
+            order: 'desc',
+          },
+          (err, r) => (err ? reject(err) : resolve(r)),
+        ),
+      );
+
+      ctx.log(
+        ctx.log.INFO,
+        `Find query in ${fakeModel.modelName}`,
         {
-          _id: new RegExp(ctx.query.search, 'i'),
+          filters: ctx.filters, offset, limit,
         },
-        {
-          name: new RegExp(ctx.query.search, 'i'),
-        },
-      ];
-    }
-    const { offset, limit } = ctx.parseRangePagination(Poi);
+      );
 
-    const { mongodb: data } = await new Promise(
-      (resolve, reject) => logger.query(
-        {
-          limit,
-          start: offset,
-          order: 'desc',
-        },
-        (err, r) => (err ? reject(err) : resolve(r)),
-      ),
-    );
+      ctx.setRangePagination(fakeModel, {
+        offset,
+        limit,
+      });
 
-    ctx.setRangePagination(Poi, {
-      offset,
-      limit,
-    });
-
-    ctx.body = data;
+      ctx.body = data;
+    },
   },
-);
+});
 
 export default router.routes();
