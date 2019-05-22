dfd813edba88254157aaf6cf6b996c6b168ee77b
Gaël <gael+git@demette.fr>
2019-05-22 18:50:54 +0200

    Create abstract test for unicity

--- tests/routes/campuses.test.mjs
+++ tests/routes/campuses.test.mjs
@@ -1,44 +1,22 @@
 import chai from 'chai';
 import request, { generateSuperAdminJWTHeader, generateRegulatorJWTHeader } from '../request';
 import Campus, { generateDummyCampus } from '../models/campus';
+import { testCreateUnicity } from '../helpers/abstract-route';
 
 const { expect } = chai;
 
+const config = {
+  route: '/campuses',
+};
+
 describe('Test the campus route', () => {
-  it('POST API endpoint should return an error when the campus already exists', async () => {
-    const dummyCampus = new Campus(generateDummyCampus());
-    try {
-      {
-        const { body: campus, statusCode } = await request()
-          .post('/campuses')
-          .set(...generateSuperAdminJWTHeader())
-          .query({ mask: 'id,name' })
-          .send({
-            id: dummyCampus._id,
-            name: dummyCampus.name,
-          });
-        expect(statusCode).to.equal(200);
-        expect(campus.id).to.equal(dummyCampus._id);
-        expect(campus.name).to.equal(dummyCampus.name);
-        const campusFound = await Campus
-          .findById(dummyCampus._id)
-          .lean();
-        expect(campusFound).to.not.be.null;
-      }
-      {
-        const { statusCode } = await request()
-          .post('/campuses')
-          .set(...generateSuperAdminJWTHeader())
-          .send({
-            id: dummyCampus._id,
-            name: dummyCampus.name,
-          });
-        expect(statusCode).to.equal(409);
-      }
-    } finally {
-      await dummyCampus.remove();
-    }
-  });
+  it(...testCreateUnicity(Campus, {
+    ...config,
+    dummy: generateDummyCampus,
+    requestCallBack: r => r
+      .set(...generateSuperAdminJWTHeader()),
+    transformObject: { id: '_id', name: 'name' },
+  }));
 
   it('It should response the GET method', async () => {
     const { body: list, statusCode } = await request()
