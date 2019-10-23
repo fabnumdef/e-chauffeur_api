de46e0658ebca43c637cc64dbf9c18c5a072a5c9
Gaël Demette <gael.demette+git@qonfucius.team>
2019-10-23 18:38:52 +0200

    Plug redis backend

--- server.mjs
+++ server.mjs
@@ -1,5 +1,6 @@
 import http from 'http';
 import socketIo from 'socket.io';
+import redisAdapter from 'socket.io-redis';
 import services from './services';
 import app from './app';
 import io from './io';
@@ -9,6 +10,10 @@ import config from './services/config';
   await services;
   const server = http.createServer(app.callback());
   app.io = socketIo(server);
+  const redisConfig = config.get('redis');
+  if (redisConfig && redisConfig.host) {
+    app.io.adapter(redisAdapter(redisConfig));
+  }
   io(app.io);
   server.listen(config.get('port') || 1337);
 })();
