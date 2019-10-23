c2dc5f1e509c48897d7eab585992c25830361924
Gaël Demette <gael.demette+git@qonfucius.team>
2019-10-23 18:28:40 +0200

    Add configurable port

--- server.mjs
+++ server.mjs
@@ -3,11 +3,12 @@ import socketIo from 'socket.io';
 import services from './services';
 import app from './app';
 import io from './io';
+import config from './services/config';
 
 (async () => {
   await services;
   const server = http.createServer(app.callback());
   app.io = socketIo(server);
   io(app.io);
-  server.listen(1337);
+  server.listen(config.get('port') || 1337);
 })();
