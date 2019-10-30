61188bf0fb988156c108a37d119e7cb5ccc963cd
Charles Capelli <charles.capelli@qonfucius.team>
2019-10-30 17:56:34 +0100

    added base and type in feedback email

--- routes/feedback.mjs
+++ routes/feedback.mjs
@@ -1,4 +1,5 @@
 import Router from 'koa-router';
+import _get from 'lodash.get';
 import config from '../services/config';
 import resolveRights from '../middlewares/check-rights';
 import { prepareSendMailFromTemplate } from '../services/mail';
@@ -15,12 +16,14 @@ router.post(
       state: { user: { name, email } },
     } = ctx;
 
+    const base = _get(ctx.state.user, 'roles[0].campuses[0].name', 'NC');
+
     const to = config.get('mail:feedback_mail');
     if (!message || !type) {
       ctx.throw_and_log(400, 'Feedback message and type should be set');
     }
 
-    const subject = `[Feedback] sent by ${name}`;
+    const subject = `[Feedback][${type}][Base: ${base}] sent by ${name}`;
 
     const sendFeedbackMail = prepareSendMailFromTemplate(
       'feedback',
