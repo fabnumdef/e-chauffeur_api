b5ce8f5e7e4e6cc76b746ae770b78161530f054b
Charles Capelli <charles.capelli@qonfucius.team>
2019-11-12 13:14:04 +0100

    Generic test pass for /ratings

--- tests/models/rating.mjs
+++ tests/models/rating.mjs
@@ -1,22 +1,22 @@
 import nanoid from 'nanoid';
 import mongoose from 'mongoose';
 import Rating from '../../models/rating';
+import Ride from '../../models/ride';
 
 const { Types: { ObjectId } } = mongoose;
 
-export const generateDummyRating = () => ({
-  _id: new ObjectId(),
-  ride: {
-    _id: '5dc13bfc07ab78491c3924f4',
-  },
-  uxGrade: 3,
-  recommandationGrade: 2,
-  message: `Rating from test -- ${nanoid()}`,
-});
-
-export const createDummyRating = async (...params) => {
-  const dummyRating = generateDummyRating(...params);
-  return Rating.create(dummyRating);
+export const generateDummyRating = async () => {
+  const [ride] = await Ride.find();
+  const rideId = ride._id.toString();
+  return {
+    _id: new ObjectId(),
+    ride: {
+      _id: rideId,
+    },
+    uxGrade: 0,
+    recommandationGrade: 1,
+    message: `Rating from test -- ${nanoid()}`,
+  };
 };
 
 export default Rating;
