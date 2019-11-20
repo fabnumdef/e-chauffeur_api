6385872feb7b98cdf1b9cd08efe39ce42d661ceb
Charles Capelli <charles.capelli@qonfucius.team>
2019-11-20 17:10:58 +0100

    Set back full tests on rides route

--- tests/routes/rides.test.mjs
+++ tests/routes/rides.test.mjs
@@ -2,66 +2,81 @@ import chai from 'chai';
 import Luxon from 'luxon';
 import request, { generateRegulatorJWTHeader } from '../request';
 import { createDummyCampus } from '../models/campus';
+import { createDummyCarModel } from '../models/car-model';
+import { createDummyCar } from '../models/car';
 import Ride, { generateDummyRide } from '../models/ride';
+import { generateDummyUser } from '../models/user';
 import { createDummyPoi } from '../models/poi';
+import { generateDummyCategory } from '../models/category';
 
 const { DateTime, Duration } = Luxon;
-const date = DateTime.local();
+const tenMinutes = Duration.fromObject({ minutes: 10 });
 const oneDay = Duration.fromObject({ days: 1 });
 const { expect } = chai;
-let dummyCampus;
-let toDropAfter = [];
 
 describe('Test the rides route', () => {
-  before(async () => {
-    dummyCampus = await createDummyCampus();
-    const dummyDeparture = await createDummyPoi();
-    const dummyArrival = await createDummyPoi();
-    const newRide = generateDummyRide({
-      start: new Date(),
-      end: new Date(),
-      campus: dummyCampus,
-      departure: dummyDeparture,
-      arrival: dummyArrival,
-    });
-    const dummyRide = await Ride.create(newRide);
-    toDropAfter = [
-      dummyCampus,
-      dummyDeparture,
-      dummyArrival,
-      dummyRide,
-    ];
-  });
-
   it('It should response the GET method', async () => {
-    const query = {
-      mask: 'id',
-      filters: {
-        campus: dummyCampus._id,
-        start: date.minus(oneDay).toJSDate(),
-        end: date.plus(oneDay).toJSDate(),
-      },
-    };
-    {
-      const response = await request()
-        .get('/rides')
-        .set('Accept', 'text/csv')
-        .set(...generateRegulatorJWTHeader())
-        .query(query);
-      expect(response.statusCode).to.equal(200);
-      expect(response.headers['content-type']).to.contain('text/csv');
-    }
-    {
-      const response = await request()
-        .get('/rides')
-        .set(...generateRegulatorJWTHeader())
-        .query(query);
-      expect(response.statusCode).to.equal(200);
-      expect(response.headers['content-type']).to.contain('application/json');
-    }
-  });
+    const toDropLater = [];
 
-  after(async () => {
-    await Promise.all(toDropAfter.map((entity) => entity.remove()));
+    try {
+      const dummyCampus = await createDummyCampus();
+      toDropLater.push(dummyCampus);
+
+
+      const dummyCarModel = await createDummyCarModel();
+      toDropLater.push(dummyCarModel);
+
+      const dummyCar = await createDummyCar({ model: dummyCarModel, campus: dummyCampus });
+      toDropLater.push(dummyCar);
+
+      const dummyDeparture = await createDummyPoi();
+      const dummyArrival = await createDummyPoi();
+      toDropLater.push(dummyDeparture);
+      toDropLater.push(dummyArrival);
+
+      const dummyDriver = generateDummyUser();
+      const dummyCategory = generateDummyCategory();
+      const date = DateTime.local();
+      const dummyRide = generateDummyRide({
+        campus: dummyCampus,
+        car: dummyCar,
+        driver: dummyDriver,
+        departure: dummyDeparture,
+        arrival: dummyArrival,
+        category: dummyCategory,
+        start: date.minus(tenMinutes).toJSDate(),
+        end: date.plus(tenMinutes).toJSDate(),
+      });
+      const rideModel = new Ride(dummyRide);
+      const savedModel = await rideModel.save();
+      toDropLater.push(savedModel);
+      const query = {
+        mask: 'id',
+        filters: {
+          campus: dummyCampus._id,
+          start: date.minus(oneDay).toJSDate(),
+          end: date.plus(oneDay).toJSDate(),
+        },
+      };
+      {
+        const response = await request()
+          .get('/rides')
+          .set('Accept', 'text/csv')
+          .set(...generateRegulatorJWTHeader())
+          .query(query);
+        expect(response.statusCode).to.equal(200);
+        expect(response.headers['content-type']).to.contain('text/csv');
+      }
+      {
+        const response = await request()
+          .get('/rides')
+          .set(...generateRegulatorJWTHeader())
+          .query(query);
+        expect(response.statusCode).to.equal(200);
+        expect(response.headers['content-type']).to.contain('application/json');
+      }
+    } finally {
+      await Promise.all(toDropLater.map((entity) => entity.remove()));
+    }
   });
 });
