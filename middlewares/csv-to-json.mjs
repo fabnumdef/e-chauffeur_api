04835fe672fe986970ad2244364c00881e097986
Charles Capelli <charles.capelli@qonfucius.team>
2020-01-16 10:50:12 +0100

    Added query params handling to csv-to-json.mjs

--- middlewares/csv-to-json.mjs
+++ middlewares/csv-to-json.mjs
@@ -3,16 +3,9 @@ import Campus from '../models/campus';
 
 export const checkDuplications = (Model, ref) => async (ctx, next) => {
   const documents = await Model.find().lean();
-  const duplications = documents.filter((document) => {
-    // eslint-disable-next-line consistent-return
-    ctx.file.forEach((data) => {
-      if (data[ref] === document[ref]) {
-        return true;
-      }
-    });
-    return false;
-  });
-
+  const duplications = documents.filter((document) => ctx.file.reduce(
+    (acc, data) => (acc || data[ref] === document[ref]), false,
+  ));
   if (duplications.length > 0) {
     ctx.throw_and_log(422, 'Duplications in the batch');
   }
@@ -40,8 +33,7 @@ export const validateCampus = async (ctx, next) => {
 };
 
 export const csvToJson = async (ctx, next) => {
-  const { body: { delimiter = ';', ignoreEmpty = true }, files } = ctx.request;
-
+  const { query: { delimiter = ';', ignoreEmpty = true }, files } = ctx.request;
   const { length, 0: file } = Object.keys(files).map((key) => files[key]);
   if (length > 1) {
     ctx.throw_and_log(422, 'Please send one file at a time');
