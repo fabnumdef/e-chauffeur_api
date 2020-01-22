e683b3ba9a9ae00127f52637bb502ee3706432e7
Gaël Demette <gael.demette+git@qonfucius.team>
2020-01-22 18:21:38 +0100

    Open port to export metrics

---
+++ prometheus-exporter.mjs
@@ -0,0 +1,11 @@
+import Prometheus from 'prom-client';
+import http from 'http';
+import config from './services/config';
+
+const port = config.get('prometheus_exporter');
+if (port) {
+  http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': Prometheus.register.contentType });
+    res.end(Prometheus.register.metrics());
+  }).listen(port || 9090);
+}
