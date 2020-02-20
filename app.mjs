2f9897009ff91e4af9883a31045113cba9cc7964
Gaël Demette <gael.demette+git@qonfucius.team>
2020-02-20 22:11:15 +0100

    Enforce Cors rule and add helmet

--- app.mjs
+++ app.mjs
@@ -1,6 +1,8 @@
+import url from 'url';
 import Koa from 'koa';
 import Prometheus from 'prom-client';
 import bodyParser from 'koa-body';
+import helmet from 'koa-helmet';
 import qsParser from 'koa-qs';
 import jwt from 'koa-jwt';
 import cors from '@koa/cors';
@@ -23,16 +25,29 @@ if (config.get('prometheus_exporter')) {
   app.use(metricsMiddleware);
 }
 app.use(errorHandler);
+app.use(helmet());
 app.use(jwt({ secret: config.get('token:secret'), passthrough: true }));
 app.use(injectUserMayMiddleware);
 app.use(loggerMiddleware);
 qsParser(app);
 app.use(rangePaginationMiddleware);
+let origin;
+const userWebsiteDomain = config.get('user_website_url');
+if (userWebsiteDomain) {
+  const { host } = url.parse(userWebsiteDomain, false);
+  origin = (ctx) => {
+    if (ctx.headers.host && ctx.headers.host.endsWith(host)) {
+      return ctx.headers.host;
+    }
+    return host;
+  };
+}
 app.use(cors({
+  origin,
   exposeHeaders: [HEADER_CONTENT_RANGES, HEADER_ACCEPT_RANGES],
 })); // @todo: fine tune, for security
 app.use(bodyParser({ multipart: true }));
-app
-  .use(routes);
+app.use(routes.routes());
+app.use(routes.allowedMethods());
 
 export default app;
