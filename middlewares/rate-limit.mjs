eca12b04af96578677684555534aff338d73da0b
Charles Capelli <charles.capelli@qonfucius.team>
2020-02-25 21:06:18 +0100

    Delete increment and use countDocuments with TTL

---
+++ middlewares/rate-limit.mjs
@@ -0,0 +1,17 @@
+import config from '../services/config';
+import RateLimit from '../models/rate-limit';
+
+export default async (ctx, next) => {
+  const { body, ip } = ctx.request;
+  if (body && body.email) {
+    const ref = body.email;
+    const userRateLimit = await RateLimit.countDocuments({ ref, ip });
+    if (userRateLimit && userRateLimit >= config.get('rate_limit:attempt_number')) {
+      ctx.throw_and_log(429, `Too many request for ${ref} with ip : ${ip}`);
+    }
+  } else {
+    ctx.throw_and_log(422);
+  }
+
+  await next();
+};
--- models/rate-limit.mjs
+++ models/rate-limit.mjs
@@ -10,37 +10,16 @@ const RateLimitSchema = new Schema({
     type: String,
     required: true,
   },
-  counter: {
-    type: Number,
-    default: 0,
-  },
   ip: {
     type: String,
     required: true,
-  },
-  locked: {
-    type: Date,
   },
 });
 
 RateLimitSchema.plugin(createdAtPlugin);
 RateLimitSchema.index(
-  { locked: 1 },
-  { expireAfterSeconds: config.get('rate_limit:lock_duration') },
-);
-RateLimitSchema.index(
   { createdAt: 1 },
   { expireAfterSeconds: config.get('rate_limit:lifespan') },
 );
-
-RateLimitSchema.methods.increment = async function increment(ref, ip) {
-  if (ref === this.ref && ip === this.ip) {
-    this.counter += 1;
-    if (this.counter === config.get('rate_limit:attempt_number')) {
-      this.locked = new Date();
-    }
-  }
-  await this.save();
-};
 
 export default mongoose.model(MODEL_NAME, RateLimitSchema);
