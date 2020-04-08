e592f5e4ad0acc3f6af8ce984efb72bbd116042c
Gaël Demette <gael.demette+git@qonfucius.team>
2020-04-08 18:22:16 +0200

    Better error management in feedback endpoint

--- middlewares/error-handler.mjs
+++ middlewares/error-handler.mjs
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import lmapKeys from 'lodash.mapkeys';
+import lmapValues from 'lodash.mapvalues';
 
 const { ValidationError } = mongoose.Error;
 export default async (ctx, next) => {
@@ -8,16 +9,41 @@ export default async (ctx, next) => {
   } catch (err) {
     if (err instanceof ValidationError) {
       ctx.status = err.status || 400;
-      ctx.body = lmapKeys(err.errors, (val, key) => {
-        if (key.endsWith('._id')) {
-          return key.slice(0, -4);
-        }
-        return key;
-      });
+      ctx.body = {
+        message: ctx.translate(
+          `mongoose.errors.${err.name}`,
+          // eslint-disable-next-line no-underscore-dangle
+          { model: ctx.translate(`mongoose.models.${err._message.split(' ').shift()}`) },
+        ),
+        errors: lmapValues(lmapKeys(err.errors, (val, key) => {
+          if (key.endsWith('._id')) {
+            return key.slice(0, -4);
+          }
+          return key;
+        }), ({ path, kind }) => ({
+          path,
+          kind,
+          message: ctx.translate(`mongoose.validators.${kind}`, { field: ctx.translate(`mongoose.paths.${path}`) }),
+        })),
+      };
     } else {
       ctx.log.error(err.message, err);
       ctx.status = err.status || 500;
-      ctx.body = ctx.translate(err.message);
+      ctx.body = {
+        message: ctx.translate(err.message),
+        errors: lmapValues(lmapKeys(err.errors, (val, key) => {
+          if (key.endsWith('._id')) {
+            return key.slice(0, -4);
+          }
+          return key;
+        }), ({
+          path, kind, message,
+        }) => ({
+          path,
+          kind,
+          message: ctx.translate(message, { field: path }),
+        })),
+      };
     }
   }
 };
